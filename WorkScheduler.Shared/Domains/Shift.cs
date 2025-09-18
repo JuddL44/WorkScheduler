0b1b73e06db7f4f729f1499dfe6446ecbcae1fd0
JL <lasaterjudd@gmail.com>
2025-09-18 16:40:58 -0500

    Added User, Role, and Shift models.

---
+++ WorkScheduler.Shared/Domains/Shift.cs
@@ -0,0 +1,9 @@
+﻿public class Shift
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Comment { get; set; } = string.Empty;
+    public DateTime? StartTime { get; set; }
+    public DateTime? EndTime { get; set; }
+    public User? Assigned { get; set; }
+}
---
+++ WorkScheduler.Shared/Domains/User.cs
@@ -0,0 +1,9 @@
+﻿public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public DateTime AccountCreationDate { get; } = DateTime.UtcNow;
+    public Role Role { get; set; }
+}
